e5e6904428e871123b09e38626e1af76f0d40c13
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-16 14:37:58 +0800

    Create io.cpp

---
+++ Basic INPUT and Output/io.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	 int num1, num2, sum;
+	 cout << "hello world" << endl;
+	 cout << "BSIT/EMC" << endl;
+
+	 cout << "Input Number 1:";
+	 cin >> num1;
+	 cout << "Input Number 2:";
+	 cin >> num2;
+
+	 sum = num1 + num2;
+
+	 cout << "The Sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+	 system("PAUSE");
+	 return 0;
+}
+
