3d05478a35eff74ee5f9f958ab29b1d8b1932d64
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-27 19:53:55 +0800

    Create looping.c

---
+++ looping.c
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+		
+		int var1 = 0;
+		int var2 = 0;
+		
+		//using while statement
+		while (var1 < 3) {
+			cout << var1 << ". Looping with while statement" << endl;
+			++var1;
+		};
+		
+		//using do while statement
+		do {
+			cout << var2 << ". Looping with do while statement" << endl;
+			++var2;
+		}
+		while (var2 < 3);
+		
+		//using for statement
+		for(int var3 = 0; var3 < 3; ++var3) {
+		    cout << var3 << ". Looping with for statement" << endl;
+		}
+		return 0;
+}
