36a4c5e4272ce8d9e708395bc56040a45927948d
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-10-14 11:11:31 +0800

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+           //size  //initialized values
+  int numbers[5] = {23,11,22,33,55};
+         //Indices  [0][1][2][3][4]
+  for(int i=0; i < 5;i++){
+                   //index
+     cout << numbers[i];
+
+  }
+
+    return 0;
+}
